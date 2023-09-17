f15639350c654c43bfe937f10b10ce8bfba8d30e
AlexTrubkina <sashatru99@gmail.com>
2023-09-17 19:15:49 +0300

    feat: add formText

---
+++ src/components/ReservationForm/FormText.tsx
@@ -0,0 +1,7 @@
+import { Text } from "@chakra-ui/react";
+
+const FormText = ({ text }: { text: string }) => {
+    return <Text fontSize="lg" marginBottom="20px">{text}</Text>;
+};
+
+export default FormText;
